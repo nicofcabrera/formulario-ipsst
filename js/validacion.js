7b3552719e9447427f5c58fe098736ae4be8673b
unknown <nico.fcok@gmail.com>
2023-02-22 17:22:58 -0300

    fix validaciones con colores bootstrap

---
+++ js/validacion.js
@@ -0,0 +1,9 @@
+// console.log('funcando')
+
+const enviar = () => {
+  console.log('funcion')
+  const formulario = document.getElementById('formulario');
+  formulario.classList.add('was-validated')
+}
+
+// was-validated
